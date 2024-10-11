65deab0532271dd88e3650aafc0d44d0ad767e2f
facumasaUY <facundomasa@gmail.com>
2024-10-11 23:25:45 +0000

    Esto fue un trabajo en equipo

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,10 @@ import ReactDOM from "react-dom/client";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
 //import your own components
-import Home from "./component/home.jsx";
+import TrafficLight from "./component/trafficLight.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+ReactDOM.createRoot(document.getElementById('app')).render(<TrafficLight/>);
 
